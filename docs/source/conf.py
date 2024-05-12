6b46b1af90fa1e4380368c6c30851d86d9a9ccf7
Witek3023 <81173708+Witek3023@users.noreply.github.com>
2024-05-12 16:36:25 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'nature'
+html_theme = 'sphinix_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
