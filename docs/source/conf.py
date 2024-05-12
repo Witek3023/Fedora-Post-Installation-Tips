4371c8f3acf288c79a08cba38df2f31f57830027
Witek3023 <81173708+Witek3023@users.noreply.github.com>
2024-05-12 18:52:20 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx_rtd_dark_mode',
 ]
 
 intersphinx_mapping = {
@@ -30,27 +31,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
-html_theme_options = {
-    'style_nav_header_background': '#343131',
-    'logo_only': True,
-    'style_external_links': True,
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'includehidden': True,
-    'titles_only': False,
-    'prev_next_buttons_location': 'bottom',
-    'style_nav_header_background': '#343131',
-    'display_version': True,
-    'navigation_depth': 3,
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'style_nav_header_background': '#343131',
-    'logo_only': False,
-    'titles_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'both',
-}
+default_dark_mode = True
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
